c5d8479096fd0a28955da2a2d555f6b42ba6cea1
maxbashnia <39924421+maxbashnia@users.noreply.github.com>
2018-06-04 23:50:24 +0300

    Update MainForm.cs

--- Archiever/MainForm.cs
+++ Archiever/MainForm.cs
@@ -14,7 +14,7 @@ namespace Archiever
 {
     public partial class MainForm : Form
     {
-        // обрані файли зберігаються в списку
+       
         List<string> selectedFiles;
 
         public MainForm()
@@ -23,7 +23,7 @@ namespace Archiever
             selectedFiles = new List<string>();
         }
 
-        // функція для обрання файлів
+      
         private void BUT_OpenFile_Click(object sender, EventArgs e)
         {
             OpenFileDialog choofdlog = new OpenFileDialog();
@@ -45,7 +45,7 @@ namespace Archiever
                 }
             }
         }
-        // функція для обрання директорії для архівування файлів
+        
         private void BUT_Archive_Click(object sender, EventArgs e)
         {
             string defaultPathOfNewFolder = @"c:\example\start";
@@ -66,25 +66,25 @@ namespace Archiever
 
             try
             {
-                // створюємо папку
+               
                 Directory.CreateDirectory(defaultPathOfNewFolder);
 
-                // копіюємо обрані файли в нову папку
+                
                 foreach (var file in this.selectedFiles)
                 {
                     File.Copy(file, Path.Combine(defaultPathOfNewFolder, file.Split('\\').Last()));
                 }
 
-                // робимо архів з нової папки
+               
                 ZipFile.CreateFromDirectory(defaultPathOfNewFolder, defaultPathOfZippedFile);
 
-                // повідомлення про успішне завершення архівації
+                
                 MessageBox.Show("Zip archieve was created successfuly");
 
-                // очищаємо список з обраними елементами після завершення архівації
+                
                 this.LB_SelectedItems.Items.Clear();
             }
-            catch (Exception ex) // якщо введені неправильні данні - обробляємо помилку та виводимо повідомлення користувачу
+            catch (Exception ex) 
             {
                 MessageBox.Show(ex.Message, "Error has occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
@@ -92,7 +92,7 @@ namespace Archiever
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            // нічого не робимо при завантажені форми
+          
         }
     }
 }
